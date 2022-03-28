51f1d870a26525df805096ef9b1cc8f440f92782
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-29 01:04:45 +0600

    Data

--- src/components/PageHead/PageHead.js
+++ src/components/PageHead/PageHead.js
@@ -2,9 +2,15 @@ import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 
 const PageHead = () => {
+  const handleBackBtn = () => {
+    window.history.back();
+  };
   return (
     <div className="flex">
-      <div className="w-10 h-10 rounded-xl bg-primaryLight flex justify-center items-center shadow shadow-primary">
+      <div
+        onClick={handleBackBtn}
+        className="cursor-pointer w-10 h-10 rounded-xl bg-primaryLight flex justify-center items-center shadow shadow-primary"
+      >
         <IoIosArrowBack className="text-primary" />
       </div>
     </div>
